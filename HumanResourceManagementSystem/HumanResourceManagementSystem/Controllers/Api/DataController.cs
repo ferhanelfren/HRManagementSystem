f3c7469d5a4b12f42d2baec22866eb416e8eec02
franconeilglovasa <68049032+franconeilglovasa@users.noreply.github.com>
2023-05-08 13:38:44 +0800

    commit

---
+++ HumanResourceManagementSystem/HumanResourceManagementSystem/Controllers/Api/DataController.cs
@@ -0,0 +1,27 @@
+﻿using HRManagementSystem_MVC_.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HumanResourceManagementSystem.Controllers.Api
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DataController : ControllerBase
+	{
+		public DataController()
+		{
+				
+		}
+
+		[HttpGet]
+
+		public EmployeeEntity GetEmployee() {
+			return new EmployeeEntity
+			{
+				Id = 1,
+				FName = "Fist Name",
+				LName = "Last Name"
+			};
+		}
+	}
+}
--- HumanResourceManagementSystem/HumanResourceManagementSystem/Program.cs
+++ HumanResourceManagementSystem/HumanResourceManagementSystem/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllersWithViews();
 
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>(opt =>
 {
@@ -55,8 +55,10 @@ if (app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	//app.UseSwagger();
+	//app.UseSwaggerUI();
+	app.UseOpenApi(); // serve OpenAPI/Swagger documents
+	app.UseSwaggerUi3();
 }
 
 app.UseHttpsRedirection();
